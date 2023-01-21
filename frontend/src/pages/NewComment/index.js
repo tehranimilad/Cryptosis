dd207bfdee37dadcaca9498d30c5c7460dc43e5d
Milad Tehrani <tehranimilad92@gmail.com>
2023-01-20 16:39:51 -0800

    fixing routing issue with nav

--- frontend/src/pages/NewComment/index.js
+++ frontend/src/pages/NewComment/index.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { createComments, getAllComments } from '../../utils/api';
 import './newComment.css'
-import { Image } from 'react-bootstrap';
+
 
 
 
