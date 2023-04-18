4b7892bea1897ca6b64de43ddabb5ae34d54929e
Milad Tehrani <114964227+tehranimilad@users.noreply.github.com>
2023-04-18 15:53:56 -0700

    Update index.js
    adding class names to style content on home page.

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -38,11 +38,11 @@ const HomePage = (props) => {
               return(
                   <Stack gap={2} className="commentSection" key={i}>
                       <div className="bg-light border">
-                        <p className="commentContent">Title: {comment.title}</p>
-                        <p className="commentContent">Username: {comment.user && comment.user.username}</p>
-                        <p className="commentContent">Time/Date Posted: {comment.created}</p>
-                        <p className="commentContent">Crypto: {comment.crypto} </p>
-                        <p className="commentContent">Comment: {comment.comment}</p>
+                        <p className="commentBold">Title: </p> <p className="commentContent">{comment.title}</p>
+                        <p className="commentBold">Username: </p> <p className="commentContent">{comment.user && comment.user.username}</p>
+                        <p className="commentBold">Time/Date Posted: </p> <p className="commentContent">{comment.created}</p>
+                        <p className="commentBold">Crypto: </p> <p className="commentContent">{comment.crypto} </p>
+                        <p className="commentBold">Comment: </p> <p className="commentContent">{comment.comment}</p>
                       </div>
                   </Stack> 
               );
